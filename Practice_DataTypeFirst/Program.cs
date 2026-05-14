64191958ab02db83e9eb297cebf06b9a34b54a96
UserHome <UserHome@DESKTOP-3494CL7>
2026-05-14 23:34:02 +0300

    Добавьте файлы проекта.

---
+++ Practice_DataTypeFirst/Program.cs
@@ -0,0 +1,32 @@
+﻿using System.Text;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine(СalculationOfCompoundInterest(1000, 3, 10));
+    }
+
+
+    static string СalculationOfCompoundInterest(int initial_deposit, int years, int interest_rate)
+    {
+
+        if ((initial_deposit <= 0) || (years <= 0) || (interest_rate <= 0))
+        {
+            return "Ошибка: Переданы неверные данные для расчета!";
+        }
+
+        double deposit = initial_deposit;
+        double currentRate = interest_rate / 100.0;
+        var CalculationInfo = new StringBuilder();
+        for (int i = 1; i < years+1; i++)
+        {
+            deposit = deposit + (deposit * currentRate);
+            CalculationInfo.AppendLine($"Год {i}: {deposit:F2} руб.");
+
+        }
+
+        return CalculationInfo.ToString();
+    }
+
+}
---
+++ Practice_DataTypeSecond/Program.cs
@@ -0,0 +1,44 @@
+﻿using System.Text;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        DrawingDiamond(5);
+    }
+
+
+    static void DrawingDiamond(int diagonalLength)
+    {
+        if ((diagonalLength % 2 == 0) || (diagonalLength < 0) )
+        {
+            Console.WriteLine("Длины диагоналей должны быть нечётные и положительные");
+            return;
+        }
+        int center = diagonalLength / 2;
+        var drawing = new StringBuilder();
+
+        for (int i = 0; i < diagonalLength; i++)
+        {
+            for (int j = 0; j < diagonalLength; j++)
+            {
+                if (i == center && j == center)
+                {
+                    drawing.Append(' ');
+                    continue;
+                }
+                if (Math.Abs(i - center) + Math.Abs(j - center) == center)
+                {
+                    drawing.Append('X');
+                }
+                else
+                {
+                    drawing.Append(' ');
+                }
+            }
+            drawing.AppendLine();
+        }
+        Console.Write(drawing.ToString());
+    }
+
+}
